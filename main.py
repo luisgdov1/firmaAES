f5d120acd6021085a829f51405247575bcf4ed19
luisgdov1 <55296930+luisgdov1@users.noreply.github.com>
2021-12-12 13:12:58 -0600

    cambio final

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+import random
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.PublicKey import RSA
@@ -16,20 +18,32 @@ def txtToBytes(ruta):
     archivo.close()
     return texto
 
+def generarLlave():
+    caracteres = list(string.ascii_letters + string.digits)
+    random.shuffle(caracteres)
+    llave_x = []
+    for i in range(16):
+        llave_x.append(random.choice(caracteres))
+    llave = "".join(llave_x)
+    return llave
 ####CIFRADO Y FIRMA#######
 
 
 ##RECIBE rutaMensajeNoFirmado == DONDE ESTA EL TXT ORIGINAL, rutaLlavePrivada == DONDE ESTA LA LLAVE PRIVADA, rutaArchivoFirmado == DONDE SE GUARDARA
-##EL ARCHIVO YA FIRMADO y LLAVE= la llave de 16 bytes que genera
-def firma(rutaMensajeNoFirmado, rutaLlavePrivada, rutaNuevoArchivoFirmado, llave):
+##EL ARCHIVO YA FIRMADO, rutaLlaveAGuardar == Ruta donde se va a guardar la llave.
+def firma(rutaMensajeNoFirmado, rutaLlavePrivada, rutaNuevoArchivoFirmado, rutaLlaveAGuardar):
     message = txtToBytes(rutaMensajeNoFirmado)
+    #Genera una llave de tamaño 16
+    llave = generarLlave()
+    print("Tu llave es " + llave)
+    f = open(rutaLlaveAGuardar, "w")
+    f.write(llave)
+    f.close()
+    print("Llave guardada en: " + rutaLlaveAGuardar)
     key = RSA.import_key(open(rutaLlavePrivada).read())
     h = SHA1.new(message)
     signature = pss.new(key).sign(h)
     contenido_firma = signature + message
-    print("Tamaño firma             :" + str(len(signature)))
-    print("Tamaño mensaje           :" + str(len(message)))
-    print("Tamaño total de contenido:" + str(len(contenido_firma)))
     cifrarFirmado(llave, contenido_firma, rutaNuevoArchivoFirmado)
     print("realizado")
 
@@ -52,6 +66,8 @@ def cifrarFirmado (llave, contenido, rutaArchivoCifrado):
 
 ##DESCIFRADO Y VERIFICACION
 #AES MODO CBC
+#RECIBE LA LLAVE EN FORMATO CLARO, ES DECIR NO EN RUTA, rutaArchivoCifrado == Donde esta guardado el archivo cifrado, rutaLlavePublica == Donde esta
+#guardada la llave publica
 def descifrarFirmado(llave, rutaArchivoCifrado, rutaLlavePublica):
     llave_b = bytes(llave, 'utf-8')
     iv_b = bytes(llave[::-1], 'utf-8')
@@ -76,6 +92,14 @@ def verificarFirma(contenido, rutaLlavePublica):
     except(ValueError, TypeError):
         print("La firma no es autentica.")
 
-firma('mensaje.txt','llaveprivadaAlicia.der', 'mensajeC.txt', '1234567890123456')
 
-descifrarFirmado('1234567890123456', 'mensajeC.txt', 'llavepublicaAlicia.der')
+##FIRMA RECIBE 1. RUTA DEL MENSAJE EN CLARO 2. RUTA DE LA LLAVE PRIVADA 3. RUTA DONDE GUARDAR EL ARCHIVO CIFRADO
+# 4.RUTA DONDE GUARDAR LA LLAVE GENERADA
+firma('mensaje.txt','llaveprivadaAlicia.der', 'mensajeC.txt', 'millave.txt')
+
+
+try:
+    #RECIBE LA LLAVE EN FORMATO STRING, NO ARCHIVO . 2. RUTA DEL MENSAJE CIFRADO 3. RUTA DE LA LLAVE PUBLICA
+    descifrarFirmado('1234567890123486', 'mensajeC.txt', 'llavepublicaAlicia.der')
+except:
+    print("Contraseña no valida")
